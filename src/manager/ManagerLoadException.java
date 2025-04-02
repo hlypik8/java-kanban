2ae8cb7b0f63d3cfb3c2868f0801634e079f7ef5
hlypik <arsenyborovskoi@yandex.ru>
2025-04-02 13:21:09 +0300

    правки кодстайла

--- src/manager/ManagerLoadException.java
+++ src/manager/ManagerLoadException.java
@@ -1,7 +1,5 @@
 package manager;
 
-import java.io.IOException;
-
 public class ManagerLoadException extends RuntimeException {
     ManagerLoadException(final String message) {
         super(message);
--- src/manager/ManagerSaveException.java
+++ src/manager/ManagerSaveException.java
@@ -1,7 +1,5 @@
 package manager;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
     ManagerSaveException(final String message) {
         super(message);
