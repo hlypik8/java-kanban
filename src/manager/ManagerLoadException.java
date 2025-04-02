d7baa6e4116c8bf4ca52d6fa252a18f934139a71
hlypik <arsenyborovskoi@yandex.ru>
2025-04-02 13:19:46 +0300

    Тип ManagerSave(Load)Exception изменен на непроверяемый

--- src/manager/ManagerLoadException.java
+++ src/manager/ManagerLoadException.java
@@ -2,7 +2,7 @@ package manager;
 
 import java.io.IOException;
 
-public class ManagerLoadException extends IOException {
+public class ManagerLoadException extends RuntimeException {
     ManagerLoadException(final String message) {
         super(message);
     }
--- src/manager/ManagerSaveException.java
+++ src/manager/ManagerSaveException.java
@@ -2,7 +2,7 @@ package manager;
 
 import java.io.IOException;
 
-public class ManagerSaveException extends IOException {
+public class ManagerSaveException extends RuntimeException {
     ManagerSaveException(final String message) {
         super(message);
     }
